9d37a52c183ccf678a418c313782bc5b9c252228
naosim <fujitanao@fujitaMacBook-Pro.local>
2017-09-10 15:31:57 +0900

    change side type to class from enum

--- src/reversi/core/side.ts
+++ src/reversi/core/side.ts
@@ -1,3 +1,8 @@
-enum Side {
-  dark, light
+class Side extends StringVO {
+  static dark = new Side('dark');
+  static light = new Side('light');
+
+  isDark() { return this.getValue() == 'dark' }
+  isLight() { return this.getValue() == 'light' }
+  reverse() { return this.isDark() ? Side.light : Side.dark }
 }
